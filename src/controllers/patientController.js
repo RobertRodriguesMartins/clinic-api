08b994dc35a77e1889d303121660cd6bb5b62572
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-13 11:09:05 -0300

    FEAT<controller>: create a patient

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -16,6 +16,16 @@ const patientController = {
 
     return res.status(200).json(data);
   },
+  create: async (req, res, next) => {
+    /**
+     * @type {{fullName: string, planId: number}}
+     *
+     */
+    const patient = req.body;
+    const data = await patientService.create(patient);
+
+    return res.status(200).json(data);
+  },
 };
 
 module.exports = patientController;
