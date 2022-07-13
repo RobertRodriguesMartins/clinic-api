e0288c636cc76818dfd227e047ab8b47ba568b1d
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-12 22:23:52 -0300

    FEAT: /patients/:planId route

--- src/routes/patient.js
+++ src/routes/patient.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const patientController = require('../controllers');
 
 router.get('/', patientController.findAll);
+router.get('/:planId', patientController.findAllByPlanId);
 
 module.exports = router;
