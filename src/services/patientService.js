8a495610627ad83eebff35e6fc9baf12c3fe9ceb
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-12 22:03:56 -0300

    FEAT: findAll returns with/without associations
    - now you can use "q" who comes from query param, in order to get one
    of two "patient" associations wich are: 'surgeries' and 'plans'
    
    - surgeries: get all patients and their surgeries
    - plans: get all patients and their plans

--- src/services/patientService.js
+++ src/services/patientService.js
@@ -3,10 +3,15 @@ const patientService = {
   /**
    * @type {Promise<Array>}
    */
-  findAll: async () => {
-    const data = await models.patient.findAll({
-      include: [{ association: 'plans' }],
-    });
+  findAll: async (options) => {
+    const possibleOptions = ['surgeries', 'plans'];
+    if (possibleOptions.includes(options)) {
+      const partialData = await models.patient.findAll({
+        include: [{ association: `${options}` }],
+      });
+      return partialData;
+    }
+    const data = await models.patient.findAll();
     return data;
   },
 };
